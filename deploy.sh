76be8bb25daa5827b0c74645c17ed18731c9064e
Tae Yun <yuntjs@gmail.com>
2017-07-13 02:58:56 -0400

    add circle yml 31

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com env TAG=0.1.$CIRCLE_BUILD_NUM DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
-  sudo docker service update --image taejunyun/quotesnodejs:$TAG --force --update-delay 3s app_node
+  docker service update --image taejunyun/quotesnodejs:$TAG --force --update-delay 3s app_node
 ENDSSH
