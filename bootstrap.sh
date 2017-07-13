8014cbc84f2223222f03c4bdcdeda14da1d9530f
Tae Yun <yuntjs@gmail.com>
2017-07-13 01:35:09 -0400

    add circle yml 7

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt-get -y install software-properties-common
+sudo apt-get -y install python-boto
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible
